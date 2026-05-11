fc186532fb654887e0acfbb3cc10a00e5eec7187
lmilanal <lazic.pr141.2023@uns.ac.rs>
2026-05-11 14:11:36 +0200

    Rad sa fajlovima, ucitavanje CSV na klijentu - zadatak 5

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,6 +1,10 @@
 using System;
 using System.ServiceModel;
 using Common;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Globalization;
 
 namespace Client
 {
@@ -16,9 +20,58 @@ namespace Client
                 Console.WriteLine("Connection to the server.");
                 ISmartGridService proxy = factory.CreateChannel();
 
-                proxy.StartSession("Session_01");
+                proxy.StartSession("Bulk_Upload_Session");
 
-                Console.WriteLine("Client is ready to send data.");
+                string filePath = ConfigurationManager.AppSettings["CsvFilePath"];
+
+                if(File.Exists(filePath))
+                {
+                    Console.WriteLine("Reading dataset..");
+                    var lines = File.ReadAllLines(filePath);
+
+                    int count = 0;
+                    foreach(var line in lines.Skip(1))
+                    {
+                        if (string.IsNullOrWhiteSpace(line)) continue;
+                        string[] parts = line.Split(',');
+
+                        try
+                        {
+                            SmartGridSample sample = new SmartGridSample
+                            {
+                                Voltage = double.Parse(parts[1], CultureInfo.InvariantCulture),
+                                Current = double.Parse(parts[2], CultureInfo.InvariantCulture),
+                                Frequency = double.Parse(parts[4], CultureInfo.InvariantCulture)
+                            };
+
+                            proxy.PushSample(sample);
+                            count++;
+
+                            if(count % 100 == 0)
+                            {
+                                Console.WriteLine($"Successfully sent {count} samples.");
+                            }
+                        }
+                        catch (FaultException<ValidationFault> ex)
+                        {
+                            Console.WriteLine($"[Validation Skip] {ex.Detail.Message}");
+                        }
+                        catch (FaultException<DataFormatFault> ex)
+                        {
+                            Console.WriteLine($"[Format Skip]{ex.Detail.Details}");
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine($"[Line Error] Skipping line. Message: {ex.Message}");
+                        }
+                    }
+                    Console.WriteLine($"Total samples provessed: {count}");
+                }
+                else
+                {
+                    Console.WriteLine("Error: CSV file not fount at the specified path.");
+                }
+
                 proxy.EndSession();
                 //proba
                 //proxy.PushSample(new SmartGridSample { Voltage = -10, Current = 5, Frequency = 50 });
